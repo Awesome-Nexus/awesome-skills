28131fa6a754e8d643c8ab76a32f6d1a00473a76
Lakshman Patel <Lakshmanp230@gmail.com>
2026-02-01 11:56:32 +0000

    fix: add custom space colors to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,37 @@
 import type { Config } from 'tailwindcss'
 
 export default <Config>{
-  content: [],
+  content: [
+    './components/**/*.{vue,js,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './composables/**/*.{js,ts}',
+    './plugins/**/*.{js,ts}',
+    './utils/**/*.{js,ts}',
+    './App.{js,ts,vue}',
+    './app.{js,ts,vue}',
+    './Error.{js,ts,vue}',
+    './error.{js,ts,vue}',
+    './assets/css/**/*.css',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        space: {
+          50: '#f8fafc',
+          100: '#f1f5f9',
+          200: '#e2e8f0',
+          300: '#cbd5e1',
+          400: '#94a3b8',
+          500: '#64748b',
+          600: '#475569',
+          700: '#334155',
+          800: '#1e293b',
+          900: '#0f172a',
+          950: '#020617',
+        },
+      },
+    },
   },
   plugins: [],
 }
